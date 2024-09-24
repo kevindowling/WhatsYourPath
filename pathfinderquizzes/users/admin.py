a268f5ab5df925755eb183123d190fd4109859bc
kevindowling <kevin.dowling@kevindowling.dev>
2024-09-24 19:10:54 -0400

    Subscription plan registered for admin site

--- pathfinderquizzes/users/admin.py
+++ pathfinderquizzes/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, SubscriptionPlan
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -14,3 +14,4 @@ class CustomUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('is_activated', 'subscription_plan', 'subscription_end_date')
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(SubscriptionPlan)
